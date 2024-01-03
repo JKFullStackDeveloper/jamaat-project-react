7909b155005c72637b17af8e44855f9883dd8511
Juned <juned.developer786@gmail.com>
2024-01-03 10:37:16 +0530

    member data added

--- TMJ-Site/src/Components/Member/Member.jsx
+++ TMJ-Site/src/Components/Member/Member.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import {member} from './MemberData.json'
 
 function Member() {
+    console.log(member[0].name)
     return (
+        <>
         <div className=" bg-cover bg-no-repeat bg-left bg-[url('../images/bg-home.jpg')]  py-16 ">
-            
+            <table style={{border:"1px solid black", width:"50%", marginLeft:"20rem"}}>
+                <thead style={{fontWeight:"bolder",textAlign:'center'}}>Member List</thead>
+                <th style={{border:"1px solid black"}}>Member Name</th>
+                <th style={{border:"1px solid black"}}>Address</th>
+                <tbody> 
+           {
+            member.map((item)=>(
+                <tr key={item.id}>
+                   <td>{item.name}</td>
+                   <td>{item.address}</td>  
+                </tr>
+            ))
+           } 
+           </tbody>
+           </table>
         </div>
+        </>
     );
 }
 
